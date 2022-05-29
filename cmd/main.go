8e18a43566bdb784ebd26ed9fdb00d46ce3bfdcd
pauloalchemist <paulolinsdev@gmail.com>
2022-05-29 19:14:29 -0300

    main changed to new api response

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/pauloalchemist/climabr/internal/cli"
 )
 
-const baseUrl = "https://apiprevmet3.inmet.gov.br/previsao/2910800"
+const baseURL = "http://apiadvisor.climatempo.com.br"
 
 func main() {
-	resp, err := http.Get(baseUrl)
+	key := os.Getenv("API_KEY")
+	if key == "" {
+		log.Fatal("Set the environment variable API_KEY.")
+	}
+
+	//Feira de Santana is 7554
+	URL := fmt.Sprintf("%s/api/v1/weather/locale/7554/current?token=%s", baseURL, key)
+
+	resp, err := http.Get(URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+	conditions, err := cli.ParseResponse(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Cidade:", conditions.City)
+	fmt.Println("Temperatura (°C):", conditions.Temp)
 }
